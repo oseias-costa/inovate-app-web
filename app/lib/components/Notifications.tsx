9de3ca567aa4f614f8fe3a1b8319ae33339b22f5
Oséias Costa <oseiasc2@gmail.com>
2024-11-19 23:43:04 -0300

    feat: include pagination

--- app/lib/components/Notifications.tsx
+++ app/lib/components/Notifications.tsx
@@ -1,69 +1,91 @@
-import Image from "next/image";
-import styled from "styled-components";
-import { List, Popover } from "antd";
-import { useQuery } from "@tanstack/react-query";
-import { httpClient } from "../utils/httpClient";
-import Logo from '@/public/assets/logo-i.png'
-import { Notification } from "../types/notification.type";
+import Image from 'next/image';
+import styled from 'styled-components';
+import { Flex, List, Popover } from 'antd';
+import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query';
+import { httpClient } from '../utils/httpClient';
+import Logo from '@/public/assets/logo-i.png';
+import { Notification } from '../types/notification.type';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { useUser } from "./UserProvider";
+import { useUser } from './UserProvider';
+import { Pagination } from '../types/pagination.type';
+import Button from 'antd/lib/button';
 
 const Content = () => {
-  const { user } = useUser()
+  const { user } = useUser();
   dayjs.extend(relativeTime);
 
+  const { data, refetch, isFetching, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery<Pagination<Notification>>({
+      queryKey: [`notification-list`],
+      queryFn: async ({ pageParam }) =>
+        httpClient({
+          path: `/notifications`,
+          method: 'GET',
+          queryString: {
+            page: pageParam,
+            limit: 4,
+            subjectUuid: user?.uuid,
+            // type: filter,
+          },
+        }),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage) => lastPage.meta.nextPage,
+    });
 
-  const { data } = useQuery({
-    queryKey: ['notifications'],
-    queryFn: async () => httpClient({
-      method: 'GET',
-      path: '/notifications',
-      queryString: {
-        subjectUuid: user?.uuid,
-        page: 1,
-        limit: 10
-      }
-    })
-  })
+  const mutate = useMutation({
+    mutationKey: ['update-notifications'],
+    mutationFn: async () =>
+      httpClient({
+        path: '/notifications',
+        method: 'PATCH',
+        queryString: {
+          userUuid: user?.uuid,
+        },
+      }),
+  });
 
   return (
     <Container>
       <List
         itemLayout="horizontal"
-        dataSource={Array.isArray(data?.items) ? data.items : []}
+        dataSource={data?.pages.flatMap((page) => page.items) || []}
         renderItem={(item: Notification, index) => (
           <List.Item>
             <A href={`/portal/request/${item.itemUuid}`}>
               <Image src={Logo} alt="Logo" width={24} height={24} />
               <div style={{ paddingLeft: 15 }}>
-                <p><strong>{item.title}</strong></p>
-                <p dangerouslySetInnerHTML={{ __html: item.description }} />
+                <p>
+                  <strong>{item.title}</strong>
+                </p>
+
                 <p>{dayjs(item.createAt).fromNow()}</p>
               </div>
             </A>
           </List.Item>
         )}
       />
+      <Flex justify="center" align="center">
+        <Button style={{ textAlign: 'center' }} onClick={() => fetchNextPage()}>
+          Carregar
+        </Button>
+      </Flex>
     </Container>
-  )
-}
+  );
+};
 
-export default function Notifications({
-  button
-}: {
-  button: JSX.Element
-}) {
+export default function Notifications({ button }: { button: JSX.Element }) {
   return (
     <Popover placement="bottomRight" title="Notificações" content={Content} trigger="click">
       {button}
     </Popover>
-
   );
 }
 
 const Container = styled.div`
   min-width: 400px;
+  max-height: 600px;
+  overflow-y: auto;
 
   @media (max-width: 840px) {
     padding-right: 20px;
@@ -72,7 +94,7 @@ const Container = styled.div`
 `;
 
 const A = styled.a`
-   text-decoration: none;
-   color: #404040;
-   display:flex;
-`
+  text-decoration: none;
+  color: #404040;
+  display: flex;
+`;
--- app/lib/types/pagination.type.ts
+++ app/lib/types/pagination.type.ts
@@ -1,14 +1,14 @@
-import { Document } from "./document.type"
+import { Document } from './document.type';
 
 export type Pagination<T> = {
-  items: T[],
-  "meta": {
-    "totalItems": number,
-    "itemCount": number,
-    "itemsPerPage": number,
-    "totalPages": number,
-    "currentPage": number,
-    "previusPage": number | null,
-    "nextPage": number | null
-  }
-}
+  items: T[];
+  meta: {
+    totalItems: number;
+    itemCount: number;
+    itemsPerPage: number;
+    totalPages: number;
+    currentPage: number;
+    previusPage: number | null;
+    nextPage: number | null;
+  };
+};
